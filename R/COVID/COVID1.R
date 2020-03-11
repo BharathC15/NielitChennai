e6d645224c4bb70773fe8300a2ecdd6afc4e92aa
BharathC15 <bharathcb@yahoo.in>
2020-03-11 11:04:02 +0530

    contigency table

---
+++ R/COVID/COVID1.R
@@ -0,0 +1,12 @@
+rawdf<-read.csv("ftp://192.168.1.15/bigdata/BigDataSet/COVID/2019_nCoV_data.csv")
+head(rawdf)
+df<-rawdf[c(4,6,7,8)]
+head(df)
+dfm<-melt(data=df,id.vars = c(1),measure.vars = c(2,3,4))
+head(dfm)
+final<-cast(dfm,formula = Country~variable,sum)
+head(final)
+row.names(final)<-final$Country
+final$Country<-NA
+final1 <- subset(final, select = c(2,3,4))
+head(final1)
