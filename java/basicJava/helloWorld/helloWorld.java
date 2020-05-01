c201513e5583a58c67a528775169e7ba2514fa18
BharathC15 <bharathcb@yahoo.in>
2020-05-01 20:54:02 +0530

    01-05-2020

---
+++ java/basicJava/helloWorld/helloWorld.java
@@ -0,0 +1,9 @@
+// To Run the code type javac <file name>
+// java <file name>
+class helloWorld 
+{ 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello, World"); 
+    } 
+} 
